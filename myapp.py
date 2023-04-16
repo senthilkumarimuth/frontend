0a6e551fbe402b779fe8b568f4000f40c34bb35f
tvs_senthil <senthilkumarimuthu@gmail.com>
2023-04-16 09:37:32 +0530

    JS for post request is added

--- myapp.py
+++ myapp.py
@@ -14,13 +14,13 @@ def home():
         lon = float(request_values['lon'])
 
         # Create Folium map centered at the given coordinates
-        m = folium.Map(location=[lat, lon], zoom_start=12)
+        m = folium.Map(location=[lat, lon], zoom_start=14)
         folium.Marker([lat, lon], popup='Your Location').add_to(m)
 
         # Render the map in the HTML template
         return m._repr_html_()
     # in GET route
-    m = folium.Map(location=[22.00963104138866, 79.10412709470455], zoom_start=12)  # india map [nagpur as center]
+    m = folium.Map(location=[22.00963104138866, 79.10412709470455], zoom_start=5)  # india map [nagpur as center]
     return render_template('index.html', map =m._repr_html_())
 
 
